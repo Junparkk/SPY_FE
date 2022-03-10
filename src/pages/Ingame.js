4ceae5c01d587904e02fcd49048e4a0b159d3cc5
Kimyangsu <kikikiju@hanmail.net>
2022-03-10 15:37:47 +0900

    0310 edit

--- src/pages/Ingame.js
+++ src/pages/Ingame.js
@@ -7,10 +7,19 @@ import { actionCreators as roomActions } from '../redux/modules/room';
 import styled from 'styled-components';
 import Draggable from 'react-draggable';
 import { useRef } from 'react';
+import { OpenVidu } from 'openvidu-browser';
 
 const socket = io.connect('http://localhost:3001');
+var OV;
+var session;
 
 function Ingame(props) {
+  //화상 채팅
+
+  OV = new OpenVidu();
+
+  session = OV.initSession();
+
   //채팅창 드레그
   const nodeRef = useRef(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -24,26 +33,30 @@ function Ingame(props) {
   const handleEnd = () => {
     setOpacity(false);
   };
-  //채팅 
+  //채팅
   const dispatch = useDispatch();
   const userId = localStorage.getItem('userid');
   const roomId = props.match.params.roomId;
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
   const [showChat, setShowChat] = useState(false);
-  const userNick = localStorage.getItem('nickname')
-  console.log(userNick)
-  console.log(roomId)
+  const userNick = localStorage.getItem('nickname');
+  console.log(userNick);
+  console.log(roomId);
 
   useEffect(() => {
     localStorage.getItem('userid');
     dispatch(roomActions.enterRoomDB(userId, roomId));
-    joinRoom()
+    joinChat();
   }, []);
 
-  const joinRoom = () => {
-      socket.emit('join_room', roomId, userNick);
-      setShowChat(true);
+  const joinChat = () => {
+    socket.emit('join_room', roomId, userNick);
+    setShowChat(true);
+  };
+
+  const joinVideo = () => {
+
   };
 
   const liveRoom = () => {
@@ -68,7 +81,7 @@ function Ingame(props) {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>참여하기</button>
+          <button onClick={joinChat}>참여하기</button>
         </div>
       </div>
       <div>
